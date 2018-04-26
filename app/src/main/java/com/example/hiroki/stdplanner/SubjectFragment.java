cd2c3af3f51706d4e46730f5e60e96e7fcbeb4ec
hiroki <hiroki10107@gmail.com>
2018-04-26 17:31:15 +0700

    initial

---
+++ app/src/main/java/com/example/hiroki/stdplanner/SubjectFragment.java
@@ -0,0 +1,30 @@
+package com.example.hiroki.stdplanner;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SubjectFragment extends Fragment {
+
+
+    public SubjectFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        ((MainActivity)getActivity()).setActionBarTitle("Subject");
+        return inflater.inflate(R.layout.fragment_subject, container, false);
+    }
+
+}
