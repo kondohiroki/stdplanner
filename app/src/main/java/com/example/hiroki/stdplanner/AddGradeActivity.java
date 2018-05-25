a4280088224650ebd36481dcdcf1b1214036574e
hiroki <hiroki10107@gmail.com>
2018-05-26 02:22:49 +0700

    addGrade

--- app/src/main/java/com/example/hiroki/stdplanner/AddGradeActivity.java
+++ app/src/main/java/com/example/hiroki/stdplanner/AddGradeActivity.java
@@ -1,13 +1,37 @@
 package com.example.hiroki.stdplanner;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddGradeActivity extends AppCompatActivity {
 
+    DatabaseHelper db;
+    Cursor c;
+    List<String> datas = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_grade);
+        Spinner spinner = (Spinner) findViewById(R.id.subject_Grade);
+
+
+    }
+    private void getData(){
+
+        db = new DatabaseHelper(this);
+        c = db.getAllSubject();
+        datas.add("Select Subject");
+        if (c != null && c.moveToFirst()) {
+            do {
+                String subject_name = c.getString(c.getColumnIndex("subject_name"));
+                datas.add(subject_name);
+            } while(c.moveToNext());
+            db.close();
+        }
     }
 }
